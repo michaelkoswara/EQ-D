356d205605ce08ea706d3f912d7c63562508ed6c
Michael Koswara <michaelkoswara@gmail.com>
2021-07-28 21:19:22 +0200

    updated test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,10 +1,18 @@
 import { render, screen, cleanup, waitFor } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
 afterEach(cleanup);
 
+jest.mock("react-router-dom", () => ({
+  ...jest.requireActual("react-router-dom"),
+  useLocation: () => ({
+    pathname: "/"
+  })
+}));
+
 test('renders the root component without crashing', async () => {
-  render(<App />);
+  render(<BrowserRouter><App /></BrowserRouter>);
   const appTitle = screen.getByText(/EQ-D/i);
   const x = await waitFor(() => screen.getByText(/EQ-D/i))
   expect(appTitle).toBeInTheDocument();
